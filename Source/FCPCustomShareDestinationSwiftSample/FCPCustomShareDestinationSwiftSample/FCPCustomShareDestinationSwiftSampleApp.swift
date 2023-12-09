d618e7e473dbb3d3f6c89ee853c0cfe27d6f72e6
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-12-10 07:57:31 +1100

    Added Initial Xcode Project
    - Added a basic Swift UI app as a starting point. Currently it does nothing.

---
+++ Source/FCPCustomShareDestinationSwiftSample/FCPCustomShareDestinationSwiftSample/FCPCustomShareDestinationSwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCPCustomShareDestinationSwiftSampleApp.swift
+//  FCPCustomShareDestinationSwiftSample
+//
+//  Created by Chris Hocking on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FCPCustomShareDestinationSwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
